62c6e7b493e6f2237d2dcb85b2c6624dbbc46d1f
EdsonMiguel <emigueltec@gmail.com>
2021-08-01 20:01:04 -0300

    Renderizando Lista de categorias

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { useTheme } from 'styled-components';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 import { MaterialIcons } from '@expo/vector-icons'
-import {Platform} from 'react-native'
+import { Platform } from 'react-native'
 
-const {Navigator, Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
-export function AppRoutes(){
+export function AppRoutes() {
   const theme = useTheme();
 
-  return(
+  return (
     <Navigator
       tabBarOptions={{
         activeTintColor: theme.colors.secundary,
         inactiveTintColor: theme.colors.text,
         labelPosition: 'beside-icon',
-        style:{
+        style: {
           paddingVertical: Platform.OS === 'ios' ? 20 : 0,
           height: 88
         }
       }}
     >
-      <Screen 
-        name="Listagem" 
+      <Screen
+        name="Listagem"
         component={Dashboard}
         options={{
-          tabBarIcon:(({size, color})=>(
-            <MaterialIcons 
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
               name="format-list-bulleted"
               size={size}
               color={color}
@@ -36,12 +37,12 @@ export function AppRoutes(){
           ))
         }}
       />
-      <Screen 
-        name="Cadastrar" 
+      <Screen
+        name="Cadastrar"
         component={Register}
         options={{
-          tabBarIcon:(({size, color})=>(
-            <MaterialIcons 
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
               name="attach-money"
               size={size}
               color={color}
@@ -49,12 +50,12 @@ export function AppRoutes(){
           ))
         }}
       />
-      <Screen 
-        name="Resumo" 
-        component={Register}
+      <Screen
+        name="Resumo"
+        component={Resume}
         options={{
-          tabBarIcon:(({size, color})=>(
-            <MaterialIcons 
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
               name="pie-chart"
               size={size}
               color={color}
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import { 
-  Container, 
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Container,
   LoadContainer,
   Header,
   UserWrapper,
@@ -17,19 +17,20 @@ import {
   LogoutButton
 } from './styles';
 
-import {HighLightCard} from '../../components/HighLightCard';
-import { TransactionCard, TransactionCardProps  } from '../../components/TransactionCard';
+import { HighLightCard } from '../../components/HighLightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
-import {ActivityIndicator} from 'react-native';
-import {useTheme} from 'styled-components/native'
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components/native'
 
-export interface DataListProps extends TransactionCardProps{
+export interface DataListProps extends TransactionCardProps {
   id: string;
 }
 
 interface HighlightProps {
   amount: string;
+  lastTransaction: string;
 }
 
 interface HighlightData {
@@ -43,18 +44,33 @@ export const Dashboard = () => {
   const [transactions, setTransactions] = useState<DataListProps[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [highlightData, setHighlightData] = useState<HighlightData>({
-    entries:{
-      amount: ""
+    entries: {
+      amount: "",
+      lastTransaction: "",
     },
-    expensives:{
-      amount: ""
+    expensives: {
+      amount: "",
+      lastTransaction: "",
     },
-    total:{
-      amount: ""
+    total: {
+      amount: "",
+      lastTransaction: "",
     },
   });
 
-  async function fetchTransactions(){
+  function getLastTransactionDate(
+    collection: DataListProps[],
+    type: 'positive' | 'negative') {
+    const lastTransaction = new Date(Math.max.apply(Math, collection
+      .filter((transaction) => transaction.type === type)
+      .map((transaction) => new Date(transaction.date).getTime())));
+
+    return `${lastTransaction.getDate()} de ${lastTransaction.toLocaleString('pt-BR', { month: 'long' })}`;
+  }
+
+
+
+  async function fetchTransactions() {
     const dataKey = "@gofinances:tramsactions";
     const response = await AsyncStorage.getItem(dataKey);
     const transactions = response ? JSON.parse(response) : [];
@@ -62,39 +78,43 @@ export const Dashboard = () => {
     let entriesTotal = 0;
     let expensiveTotal = 0;
 
-    const transactionsFormatted:DataListProps[] = transactions
-    .map((item: DataListProps) => {
+    const transactionsFormatted: DataListProps[] = transactions
+      .map((item: DataListProps) => {
 
-      if(item.type === 'positive'){
-        entriesTotal += Number(item.amount);
-      }else {
-        expensiveTotal += Number(item.amount);
-      }
+        if (item.type === 'positive') {
+          entriesTotal += Number(item.amount);
+        } else {
+          expensiveTotal += Number(item.amount);
+        }
 
-      const amount = Number(item.amount)
-      .toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
+        const amount = Number(item.amount)
+          .toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          });
+
+        const date = Intl.DateTimeFormat('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit'
+        }).format(new Date(item.date))
+
+        return {
+          id: item.id,
+          name: item.name,
+          type: item.type,
+          category: item.category,
+          amount,
+          date
+        }
       });
 
-      const date = Intl.DateTimeFormat('pt-BR',{
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit'
-      }).format(new Date(item.date))
-
-      return {
-        id: item.id,
-        name: item.name,
-        type: item.type,
-        category: item.category,
-        amount,
-        date
-      }
-    });
-    
     setTransactions(transactionsFormatted);
-    setIsLoading(false);
+
+
+    const lastTransactionEntries = getLastTransactionDate(transactions, 'positive');
+    const lastTransactionExpensives = getLastTransactionDate(transactions, 'negative');
+    const totalInterval = `01 a ${lastTransactionExpensives}`;
 
     const total = entriesTotal - expensiveTotal;
 
@@ -103,54 +123,60 @@ export const Dashboard = () => {
         amount: entriesTotal.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        })
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionEntries}`,
       },
       expensives: {
         amount: expensiveTotal.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        })
+        }),
+        lastTransaction: `Última saída dia ${lastTransactionExpensives}`,
       },
       total: {
         amount: total.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        })
+        }),
+        lastTransaction: totalInterval
       }
     });
+
+    setIsLoading(false);
+
   }
 
   useEffect(() => { fetchTransactions() }, [])
-  useFocusEffect(useCallback(()=>{fetchTransactions()},[]))
-  
+  useFocusEffect(useCallback(() => { fetchTransactions() }, []))
+
   const theme = useTheme();
 
   return (
     <Container>
       {
-        isLoading ? 
+        isLoading ?
           <LoadContainer>
-            <ActivityIndicator 
-              color={theme.colors.primary} 
+            <ActivityIndicator
+              color={theme.colors.primary}
               size="large"
-            /> 
+            />
           </LoadContainer>
-         :
+          :
           <>
             <Header>
               <UserWrapper>
                 <UserInfo>
                   <Photo
-                    source={{uri: 'https://github.com/edsonMiguel.png'}}
+                    source={{ uri: 'https://github.com/edsonMiguel.png' }}
                   />
                   <User>
                     <UserGreeting>Olá, </UserGreeting>
                     <UserName>Miguel</UserName>
                   </User>
                 </UserInfo>
-                <LogoutButton onPress={() => {}}>
-                  <Icon name="power"/>
-                </LogoutButton> 
+                <LogoutButton onPress={() => { }}>
+                  <Icon name="power" />
+                </LogoutButton>
               </UserWrapper>
             </Header>
             <HighLightCards>
@@ -158,34 +184,34 @@ export const Dashboard = () => {
                 type="up"
                 title="Entrada"
                 amount={highlightData.entries.amount}
-                lastTransaction="Última entrada dia 13 de abril"
+                lastTransaction={highlightData.entries.lastTransaction}
               />
               <HighLightCard
                 type="down"
                 title="Saídas"
                 amount={highlightData.expensives.amount}
-                lastTransaction="Última saída dia 03 de abril"
+                lastTransaction={highlightData.expensives.lastTransaction}
               />
               <HighLightCard
                 type="total"
                 title="Total"
                 amount={highlightData.total.amount}
-                lastTransaction="01 à 16 de abril"
+                lastTransaction={highlightData.total.lastTransaction}
               />
-        
+
             </HighLightCards>
             <Transactions>
               <Title>Listagem</Title>
 
-              <TransactionList 
+              <TransactionList
                 data={transactions}
                 keyExtractor={item => item.id}
-                renderItem={({item})=><TransactionCard data={item}/>}
+                renderItem={({ item }) => <TransactionCard data={item} />}
               />
-            </Transactions> 
+            </Transactions>
           </>
-       }
-    </Container>  
+      }
+    </Container>
   );
 }
 
